e6757af471bc3844c6d6ef8576ab01161a68e378
NAVEEN R.S <127390244+nrs14@users.noreply.github.com>
2023-03-28 20:31:30 +0530

    Create siso.v

---
+++ siso.v
@@ -0,0 +1,11 @@
+module siso(clk,in,rst,q);
+input clk,rst,in;
+output q;
+reg [3:0]qq;
+assign q=qq[0];
+always @ (posedge clk, posedge rst)
+begin
+if(rst) qq <=0;
+else qq <= {in,qq[3:1]};
+end
+endmodule
